a9c1b6c6697fcc22af7602994fafb10ce0b2aafa
Rodrigo Kunzler Weber <rodrigo.weber@appmax.com.br>
2025-10-22 11:16:50 -0300

    add initial implementation of the monitoring program

---
+++ hello.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+
+    exibeIntroducao()
+    exibeMenu()
+    comando := leComando()
+
+    switch comando {
+    case 1:
+        iniciarMonitoriamento()
+    case 2:
+        fmt.Println("Exibindo Logs...")
+    case 0:
+        fmt.Println("Saindo do programa...")
+		os.Exit(0)
+    default:
+        fmt.Println("Não conheço este comando")
+		os.Exit(-1)
+    }
+}
+
+func exibeIntroducao() {
+    nome := "Douglas"
+    versao := 1.1
+    fmt.Println("Olá, sr.", nome)
+    fmt.Println("Este programa está na versão", versao)
+}
+
+func exibeMenu() {
+    fmt.Println("1- Iniciar Monitoramento")
+    fmt.Println("2- Exibir Logs")
+    fmt.Println("0- Sair do Programa")
+}
+
+func leComando() int {
+    var comandoLido int
+    fmt.Scan(&comandoLido)
+    fmt.Println("O comando escolhido foi", comandoLido)
+
+    return comandoLido
+}
+
+func iniciarMonitoriamento() {
+	fmt.Println("Monitorando...")
+	site:= "https://www.alura.com.br" 
+
+	resp, _ := http.Get(site)
+	
+    if resp.StatusCode == 200 {
+        fmt.Println("Site:", site, "foi carregado com sucesso!")
+    } else {
+        fmt.Println("Site:", site, "está com problemas. Status Code:", resp.StatusCode)
+    }
+}
