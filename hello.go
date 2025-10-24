835f19c7ed96bd0b12c1800ad0a0764e567cd0c8
Rodrigo Kunzler Weber <rodrigo.weber@appmax.com.br>
2025-10-24 10:55:04 -0300

    refactor main function to improve command handling and structure

--- hello.go
+++ hello.go
@@ -7,23 +7,26 @@ import (
 )
 
 func main() {
-
     exibeIntroducao()
-    exibeMenu()
-    comando := leComando()
 
-    switch comando {
-    case 1:
-        iniciarMonitoriamento()
-    case 2:
-        fmt.Println("Exibindo Logs...")
-    case 0:
-        fmt.Println("Saindo do programa...")
-		os.Exit(0)
-    default:
-        fmt.Println("Não conheço este comando")
-		os.Exit(-1)
+    for {
+        exibeMenu()
+        comando := leComando()
+
+        switch comando {
+        case 1:
+            iniciarMonitoriamento()
+        case 2:
+            fmt.Println("Exibindo Logs...")
+        case 0:
+            fmt.Println("Saindo do programa...")
+            os.Exit(0)
+        default:
+            fmt.Println("Não conheço este comando")
+            os.Exit(-1)
+        }
     }
+
 }
 
 func exibeIntroducao() {
@@ -49,6 +52,13 @@ func leComando() int {
 
 func iniciarMonitoriamento() {
 	fmt.Println("Monitorando...")
+
+    var sites [2]string
+    sites[0] = "https://www.alura.com.br"
+    sites[1] = "https://www.caelum.com.br"
+
+    fmt.Println(sites)
+
 	site:= "https://www.alura.com.br" 
 
 	resp, _ := http.Get(site)
